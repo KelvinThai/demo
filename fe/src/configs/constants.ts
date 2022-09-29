5d98d3905564e51278f14872d4dd54cfd8323a6a
doclt <thongdoc0508@gmail.com>
2022-09-29 10:26:21 +0700

    remove home menu

--- fe/src/configs/constants.ts
+++ fe/src/configs/constants.ts
@@ -15,8 +15,8 @@ export const fonts = {
 }
 
 export const menus = [
-  { name: "Home", url: "/" },
-  { name: "Market", url: "/" },
+  // { name: "Home", url: "/" },
+  // { name: "Market", url: "/" },
   { name: "Invest", url: "/invest" },
   { name: "Deposit/Withdraw", url: "deposit-withdraw" },
   { name: "Staking", url: "/staking" },
--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Text, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import MainLayout from '../layouts'
-import MarketView from '../views/Markets'
+import InvestView from '../views/Invests'
+//import MarketView from '../views/Markets'
 import ConvertTicket from '../views/Vaults/ConvertTicket'
 
 const Home: NextPage = () => {
-  return <MarketView />
+  return <InvestView />
 }
 
 export default Home
