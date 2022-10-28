300fd19fcbece85c464aef77913337139b9babca
doclt <thongdoc0508@gmail.com>
2022-10-28 15:53:29 +0700

    add conect infor into .env

--- api/vaulConnectionPool.js
+++ api/vaulConnectionPool.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 const poolConfig = {
-    host: '13.213.0.113',
-    port: 3306,
-    user: 'dequest',
-    password: 'DQ@123456',
-    database: 'Offchain',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.VAULT_DB_NAME,
     waitForConnections: true,
     connectionLimit: 50
 };
